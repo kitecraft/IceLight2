8e54b46bbe2f8f11e9ab7fe29a07f5aeff43515f
Kitecraft Unborn <kitecraft@gmail.com>
2023-04-09 12:18:21 -0600

    Tweak the preferences funcs

--- software/Icelight/main/src/Preferences/IceLight_NVS_Keys.h
+++ software/Icelight/main/src/Preferences/IceLight_NVS_Keys.h
@@ -1,9 +1,15 @@
 
-const char *IL_PREF_NS_GENERAL = "ICELIGHT";
+//IceLight General NVS Namespace
+const char *IL_PREF_NAMESPACE_GENERAL = "ICELIGHT";
+//IceLight general keys
 const char *IL_PREF_KEY_DEVICE_NAME = "DNAME";
-const char *IL_PREF_KEY_LAST_BRIGHTNESS = "BNESS";
-const char *IL_PREF_KEY_LAST_ACTION = "LACTION";
-const char *IL_PREF_KEY_LAST_ACTION_VALUE = "LAVALUE";
 
-const char *IL_PREF_NS_WIFI = "WIFI";
+//IceLight LED namespace
+const char *IL_PREF_NAMESPACE_LED = "ICELED";
+//IceLight LED keys
+const char *IL_PREF_KEY_LAST_BRIGHTNESS = "BNESS";
+
+//IceLight Wifi namespace
+const char *IL_PREF_NAMESPACEF_WIFI = "WIFI";
+//IceLight Wifi keys
 const char *IL_PREF_KEY_STA_ENABLED = "WIFI_STA";
